effdfaac0d716bdac04e46aacfcf5a7c61de10bd
f3kilo <f3kilo@yandex.ru>
2023-04-11 17:11:49 +0300

    cpu time calculated

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use std::{
+    thread,
+    time::{Duration, Instant},
+};
+
 mod time;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Thread sleeps:");
+    for _ in 0..5 {
+        thread::sleep(Duration::from_secs(1));
+        let ts = unsafe { time::clock() };
+        println!("Timestamp: {ts};");
+    }
+
+    println!("Thread works:");
+    for _ in 0..5 {
+        let instant = Instant::now();
+        while Instant::now() < instant + Duration::from_secs(1) {}
+
+        let ts = unsafe { time::clock() };
+        println!("Timestamp: {ts};");
+    }
 }
